e44ab0d29117e21a9b42d6506281e1fe4e3fed45
SMH <38455739+smh997@users.noreply.github.com>
2021-06-27 18:35:17 +0430

    #4 phone number has some changes.

--- bilche_backend/accounts/models.py
+++ bilche_backend/accounts/models.py
@@ -12,10 +12,10 @@ class User(AbstractUser):
     fullname = models.CharField(max_length=100, blank=True, default='')
     level = models.CharField(max_length=1, choices=level_choices, default='b')
     profile_photo = models.ImageField(null=True, blank=True, upload_to='profile_photoes')
-    phone_number = PhoneField(unique=True, blank=True, null=True, error_messages={
-        'unique': 'A user with this phone number already exists.'
+    phone_number = PhoneField(blank=True, null=True)
+    email = models.EmailField(unique=True, error_messages={
+        'unique': 'کاربری با این ایمیل وجود دارد.'
     })
-    email = models.EmailField(unique=True, blank=True, null=True)
     is_gardener = models.BooleanField(default=False)
     prefered_hour = models.DateTimeField(blank=True, null=True)
     _prefered_days = models.CharField(max_length=7, default='-------')
@@ -26,7 +26,7 @@ class User(AbstractUser):
 
 
 class Location(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='locations')
     city = models.CharField(max_length=50, blank=True, default='')
     country = models.CharField(max_length=50, blank=True, default='')
     latitude = models.FloatField(default=0)
