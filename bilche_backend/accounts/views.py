be6ac92beda44c32e447ab6bd259a1b8a98d5006
SMH <38455739+smh997@users.noreply.github.com>
2021-06-29 14:54:58 +0430

    #6 MyObtainAuthToken api added and UserRegistrationAPIView edited.

--- bilche_backend/accounts/views.py
+++ bilche_backend/accounts/views.py
@@ -1,13 +1,20 @@
+from django.contrib.auth import login
 from django.shortcuts import render
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from rest_framework.generics import CreateAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
 
-from accounts.serializers import UserSerializer
+from accounts.serializers import UserSerializer, MyAuthTokenSerializer
 from rest_framework.views import APIView
+
+
+class MyObtainAuthToken(ObtainAuthToken):
+    serializer_class = MyAuthTokenSerializer
 
 
 class UserRegistrationAPIView(CreateAPIView):
@@ -19,7 +26,16 @@ class UserRegistrationAPIView(CreateAPIView):
     def create(self, request, *args, **kwargs):
         request.data['username'] = request.data['email']
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                token, create = Token.objects.get_or_create(user=user)
+                data_response = dict()
+                data_response['user'] = serializer.data
+                data_response['token'] = token.key
+                return Response(data_response, status=status.HTTP_201_CREATED)
+        errors = serializer.errors
+        if 'username' in errors:
+            errors.pop('username')
+        return Response(data={'errors': errors}, status=status.HTTP_400_BAD_REQUEST)
+
