d23904d0f63be6122da2651626167e0cf3f0a268
SMH <38455739+smh997@users.noreply.github.com>
2021-07-01 04:12:06 +0430

    #13 serializers.py built and FeedbackSerializer, SubscribeSerializer, and InstallappSerializer implemented.

---
+++ bilche_backend/home_page/serializers.py
@@ -0,0 +1,41 @@
+import re
+
+from rest_framework import serializers
+
+from home_page.models import Bilche_feedback, Bilche_subscribe, Bilche_install_app
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bilche_feedback
+        fields = ('fullname', 'email', 'text')
+
+
+class SubscribeSerializer(serializers.Serializer):
+
+    email_phone = serializers.CharField(max_length=50)
+
+    def validate(self, data):
+        try:
+            if '@' not in data['email_phone']:
+                raise Exception('لطفا شماره تلفن همراه یا یک ایمیل معتبر وارد نمایید.'
+                                'شماره تلفن همراه باید با 09 شروع شود و تنها شامل 11 رقم باشد.')
+        except Exception as e:
+            pattern = re.compile("^09\d{9}$")
+            if pattern.match(data['email_phone']) is None:
+                raise serializers.ValidationError(e)
+        return data
+
+    def create(self, validated_data):
+        if '@' in validated_data['email_phone']:
+            return Bilche_subscribe.objects.create(email=validated_data['email_phone'])
+        return Bilche_subscribe.objects.create(phone_number=validated_data['email_phone'])
+
+    def update(self, instance, validated_data):
+        pass
+
+
+class InstallappSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bilche_install_app
+        fields = ('user', 'application_store')
