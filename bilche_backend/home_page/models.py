e8403df661943dc2ce82ecf7c6a3843f038af777
SMH <38455739+smh997@users.noreply.github.com>
2021-06-30 22:44:42 +0430

    #11 models implemented.

--- bilche_backend/home_page/models.py
+++ bilche_backend/home_page/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from phone_field import PhoneField
 
-# Create your models here.
+from accounts.models import User
+
+
+class Bilche_feedback(models.Model):
+    fullname = models.CharField(max_length=100, blank=True, default='')
+    email = models.EmailField(null=True)
+    text = models.TextField()
+
+
+class Bilche_subscribe(models.Model):
+    email = models.EmailField(null=True)
+    phone_number = PhoneField(null=True)
+
+
+class Bilche_install_app(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='installations')
+    application_store_choices = (
+        ('sa', 'Sib app'),
+        ('s', 'Sibche'),
+        ('b', 'Bazaar'),
+        ('g', 'Google play'),
+        ('d', 'Direct')
+    )
+    application_store = models.CharField(max_length=2, choices=application_store_choices)
+
