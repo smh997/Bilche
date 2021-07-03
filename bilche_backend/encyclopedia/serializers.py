fd69774e227ee6c832603df06ab10d5e511b8ef7
SMH <38455739+smh997@users.noreply.github.com>
2021-07-04 00:29:04 +0430

    #18 BasePlantSerializer changed to BasePlantListSerializer and modified. BasePlantObjectSerializer implemented for get base plan API.

--- bilche_backend/encyclopedia/serializers.py
+++ bilche_backend/encyclopedia/serializers.py
@@ -3,7 +3,44 @@ from rest_framework import serializers
 from encyclopedia.models import BasePlant
 
 
-class BasePlantSerializer(serializers.ModelSerializer):
+class BasePlantListSerializer(serializers.ModelSerializer):
+    picture = serializers.SerializerMethodField()
+
+    class Meta:
+        model = BasePlant
+        fields = ('id', 'title', 'picture')
+
+    def get_picture(self, base_plant):
+        return base_plant.pictures.first().picture.url
+
+
+class BasePlantObjectSerializer(serializers.ModelSerializer):
+    common_names = serializers.SerializerMethodField()
+    categories = serializers.SerializerMethodField()
+    pictures = serializers.SerializerMethodField()
+    waterings = serializers.SerializerMethodField()
+    fertilizers = serializers.SerializerMethodField()
+    colors = serializers.SerializerMethodField()
+
     class Meta:
         model = BasePlant
         fields = '__all__'
+
+    def get_common_names(self, base_plant):
+        return [common_name.name for common_name in base_plant.common_names.all()]
+
+    def get_categories(self, base_plant):
+        # return [category for category in (pcategory.category for pcategory in base_plant.categories.all())]
+        return [category for category in base_plant.categories.values_list('category__id', 'category__name')]
+
+    def get_pictures(self, base_plant):
+        return [image.picture.url for image in base_plant.pictures.all()]
+
+    def get_waterings(self, base_plant):
+        return [watering for watering in base_plant.waterings.values_list('season', 'range')]
+
+    def get_fertilizers(self, base_plant):
+        return [fertilizer for fertilizer in base_plant.fertilizers.values_list('base_fertilizer__name', 'season', 'range')]
+
+    def get_colors(self, base_plant):
+        return [color for color in base_plant.flower_colors.values_list('flower_color__color_code', 'flower_color__color_name')]
