04cc9ab6335e12c631114c1e8875c328e83bea4a
SMH <38455739+smh997@users.noreply.github.com>
2021-07-04 00:17:25 +0430

    #24 media directory built and pictures added, also media url added to project urls.

--- bilche_backend/bilche_backend/urls.py
+++ bilche_backend/bilche_backend/urls.py
@@ -4,6 +4,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.schemas import get_schema_view as g
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -30,3 +32,4 @@ urlpatterns = [
 
     path('api/openapi/', g(), name='openapi-schema'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
