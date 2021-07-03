05e781fa907b693fe6da4b9f5e8f292d5c1df61a
SMH <38455739+smh997@users.noreply.github.com>
2021-07-03 18:54:33 +0430

    #18 small important change in BasePlantSerializer.

--- bilche_backend/encyclopedia/serializers.py
+++ bilche_backend/encyclopedia/serializers.py
@@ -6,7 +6,4 @@ from encyclopedia.models import BasePlant
 class BasePlantSerializer(serializers.ModelSerializer):
     class Meta:
         model = BasePlant
-        fields = ('title', 'scientific_name', 'family', 'level', 'need_to_check', 'fogging', 'pruning',
-                  'cleaning_leaves', 'cleaning_pot', 'light', 'temperature', 'humidity', 'soil_type', 'toxic',
-                  'irritant', 'life_span', 'flower_time', 'leaf_time', 'max_height', 'max_width', 'flower_type',
-                  'leaf_type', 'change_pot', 'pot_type')
+        fields = '__all__'
