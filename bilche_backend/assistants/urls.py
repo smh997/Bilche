38a8863c4de7adce958ca36db1e8b9c066726aee
SMH <38455739+smh997@users.noreply.github.com>
2021-07-07 15:47:58 +0430

    #35 AddNewSiteAPIView implemented.

--- bilche_backend/assistants/urls.py
+++ bilche_backend/assistants/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from assistants.views import AddNewSiteAPIView
+
 urlpatterns = [
+    path('add_site/', AddNewSiteAPIView.as_view()),
 
 ]
--- bilche_backend/assistants/views.py
+++ bilche_backend/assistants/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+from assistants.models import Site
+from assistants.serializers import SiteSerializer, SitesListSerializer, SiteObjectSerializer
+
+
+class AddNewSiteAPIView(CreateAPIView):
+    serializer_class = SiteSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer(self, *args, **kwargs):
+        self.request.data['user'] = self.request.user.id
+        serializer_class = self.get_serializer_class()
+        kwargs.setdefault('context', self.get_serializer_context())
+        return serializer_class(*args, **kwargs)
+
