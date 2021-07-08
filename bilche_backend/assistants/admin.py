5362d891a721f7e5c5473ad52cce8ea5bb3c1eab
SMH <38455739+smh997@users.noreply.github.com>
2021-07-08 18:23:21 +0430

    #45 admin implemented.

--- bilche_backend/assistants/admin.py
+++ bilche_backend/assistants/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from assistants.models import *
+
+admin.site.register(Site)
+admin.site.register(Plant)
+admin.site.register(Activity)
